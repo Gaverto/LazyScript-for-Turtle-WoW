6a54dd90ba59b5eb0d01a73754beced6733006ce
Gaverto <48242230+Gaverto@users.noreply.github.com>
2025-10-09 21:48:42 +0100

    Update ParseBuffs.lua
    -ifNotBuffDuration and -ifNotBuffTitleDuration now accept second criteria without decimal.
    
    Example: renew-ifNotBuffDuration<5s=renew
    
    Credit to Zanthor for code contribution.

--- Addons/LazyScript/ParseBuffs.lua
+++ Addons/LazyScript/ParseBuffs.lua
@@ -676,7 +676,7 @@ end
 
 
 function lazyScript.bitParsers.ifBuffDuration(bit, actions, masks)
-	if (not lazyScript.rebit(bit, "^if(Not)?(Buff|Debuff)Duration([<>])(d%.%d+)s=(.+)$")) then
+	if (not lazyScript.rebit(bit, "^if(Not)?(Buff|Debuff)Duration([<>])(%d+%.?%d*)s=(.+)$")) then
 		return false
 	end
 	local negate = lazyScript.negate1()
@@ -1243,4 +1243,5 @@ function lazyScript.masks.FindBuffsByCategory(category)
 	end
 
 	return buffs
+
 end
